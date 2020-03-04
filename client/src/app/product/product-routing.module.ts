349800a474fe0f1fcfb0158d47277ee8591538f3
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-04 12:59:25 -0500

    modificacion rutas

--- client/src/app/product/product-routing.module.ts
+++ client/src/app/product/product-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductListComponent } from './admin/product-list/product-list.component';
-import { ProductEditorComponent } from './product-editor/product-editor.component';
-import { ProductCreatorComponent } from './product-creator/product-creator.component';
+import { ProductListComponent } from './admin/admin/product-list/product-list.component';
+import { ProductEditorComponent } from './admin/admin/product-editor/product-editor.component';
+import { ProductCreatorComponent } from './admin/admin/product-creator/product-creator.component';
 import { ProductModule } from './product.module';
+import { ProductHomeComponent } from './product-home/product-home.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   /* Rutas de producto */
 
   {
-    path: 'product/list',
+    path: 'admin/product/list',
     component: ProductListComponent
 
   },
   {
-    path: 'product/editor',
+    path: 'admin/product/editor',
     component: ProductEditorComponent
 
   },
@@ -24,8 +26,18 @@ const routes: Routes = [
     redirectTo: '/product/list'
   },
   {
-    path: 'product/creator',
+    path: 'admin/product/creator',
     component: ProductCreatorComponent
+
+  },
+  {
+    path: '/product/home',
+    component: ProductHomeComponent
+
+  },
+  {
+    path: '/product/detalles',
+    component: ProductDetailsComponent
 
   }
 ];
--- client/src/app/product/product.module.ts
+++ client/src/app/product/product.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductListComponent } from './admin/product-list/product-list.component';
-import { ProductCreatorComponent } from './product-creator/product-creator.component';
-import { ProductEditorComponent } from './product-editor/product-editor.component';
+import { ProductListComponent } from './admin/admin/product-list/product-list.component';
+import { ProductCreatorComponent } from './admin/admin/product-creator/product-creator.component';
+import { ProductEditorComponent } from './admin/admin/product-editor/product-editor.component';
 import { ProductHomeComponent } from './product-home/product-home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
@@ -16,7 +16,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   exports:[
     ProductListComponent,
     ProductCreatorComponent,
-    ProductCreatorComponent
+    ProductCreatorComponent,
+    ProductDetailsComponent,
+    ProductHomeComponent
   ]  
 })
 export class ProductModule { }
