b6b6a10358e52fd709ec6e25e3fa268e1e405c28
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-04 12:07:23 -0500

    nuevos modulos y componentes

---
+++ client/src/app/parameters/parameters.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ParametersRoutingModule } from './parameters-routing.module';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { CategoryCreatorComponent } from './category/category-creator/category-creator.component';
+import { CategoryEditorComponent } from './category/category-editor/category-editor.component';
+
+
+@NgModule({
+  declarations: [CategoryListComponent, CategoryCreatorComponent, CategoryEditorComponent],
+  imports: [
+    CommonModule,
+    ParametersRoutingModule
+  ]
+})
+export class ParametersModule { }
--- client/src/app/product/product.module.ts
+++ client/src/app/product/product.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductCreatorComponent } from './product-creator/product-creator.component';
 import { ProductEditorComponent } from './product-editor/product-editor.component';
+import { ProductHomeComponent } from './product-home/product-home.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
-  declarations: [ProductListComponent, ProductCreatorComponent, ProductEditorComponent],
+  declarations: [ProductListComponent, ProductCreatorComponent, ProductEditorComponent, ProductHomeComponent, ProductDetailsComponent],
   imports: [
     CommonModule
   ],
