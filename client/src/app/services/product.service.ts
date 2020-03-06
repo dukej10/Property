c83e3e1b13abf178b61d29968f08fd3bfc09970e
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-06 00:26:31 -0500

    import

--- client/src/app/services/product.service.ts
+++ client/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PropertyModel } from '../models/product.model';
 import { Observable} from 'rxjs';
+import { ProductModule } from '../product/product.module';
 
 
 const base_url: string = 'http://localhost:3000/api/' 
