0adfd8c9b7e54b046ff7a36cabbbaf78a03f4a8b
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-06 00:25:04 -0500

    import FormsModule

--- client/src/app/parameters/parameters.module.ts
+++ client/src/app/parameters/parameters.module.ts
@@ -5,13 +5,15 @@ import { ParametersRoutingModule } from './parameters-routing.module';
 import { CategoryListComponent } from './category/category-list/category-list.component';
 import { CategoryCreatorComponent } from './category/category-creator/category-creator.component';
 import { CategoryEditorComponent } from './category/category-editor/category-editor.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryCreatorComponent, CategoryEditorComponent],
   imports: [
     CommonModule,
-    ParametersRoutingModule
+    ParametersRoutingModule,
+    FormsModule
   ],
   exports:[
     CategoryCreatorComponent,
