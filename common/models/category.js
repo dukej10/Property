b6c26c3ac9e0591310e9fdc4e4bfa34396648b48
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-06 15:42:52 -0500

    funcion busqueda

--- common/models/category.js
+++ common/models/category.js
@@ -1,5 +1,71 @@
 'use strict';
 
 module.exports = function(Category) {
+    Category.getCategorySameName= function (name, cb){
+        Category.find({
+            where:{
+                name:{
+                    eq: name
+                 }
+            }
+        }, cb);
+    
+    };
+
+
+    Category.remoteMethod("getCategorySameName", {
+        accepts: {
+            arg: "name",
+            type: "string"
+        },
+        returns: {
+            arg: "Categories",
+            type: "array"
+        },
+        http: {
+            path: "/get-category-same-name",
+            verb: "get"
+
+        }}
+    
+    
+    )
+};
+
+/* module1.exports = function(Category) {
+Category.getCategorySameTrequest= function (trequest, cb){
+    Category.find({
+        where:{
+            trequest:{
+                eq: trequest
+                }
+        }
+    }, cb);
 
 };
+
+
+Category.remoteMethod1("getCategorySameTrequest", {
+    accepts: {
+        arg: "trequest",
+        type: "string"
+    },
+    returns: {
+        arg: "Categories",
+        type: "array"
+    },
+    http: {
+        path: "/get-category-same-trequest",
+        verb: "get"
+
+    }}
+
+
+)
+
+};
+
+
+
+
+ */
--- common/models/property.js
+++ common/models/property.js
@@ -1,5 +1,4 @@
-'use strict';
-
+ 'use strict'; 
 module.exports = function(Property) {
     Property.getProductsSameAvailable= function (available, cb){
         Property.find({
@@ -30,6 +29,83 @@ module.exports = function(Property) {
     
     
     )
+    
+    /* menor o igual precio */
+    Property.ProductsSamecategory= function (price, cb){
+        Property.find({
+            where:{
+                price:{
+                    lt: price
+                 }
+            }
+        }, cb);
+    
+    };
+
+    Property.remoteMethod("ProductsSamecategory", {
+        accepts: {
+            arg: "price",
+            type: "string"
+        },
+        returns: {
+            arg: "properties",
+            type: "array"
+        },
+        http: {
+            path: "/get-product-same-category",
+            verb: "get"
+
+        }}
+    
+    
+    )
+
+    
+
+
+
+    
+
 
 
 };
+
+/* 
+module.exports = function(Property) {
+    Property.getProductsSamecategory= function (category, cb){
+        Property.find({
+            where:{
+                category:{
+                    eq: category
+                 }
+            }
+        }, cb);
+    
+    };
+
+
+    Property.remoteMethod("getProductsSamecategory", {
+        accepts: {
+            arg: "category",
+            type: "string"
+        },
+        returns: {
+            arg: "properties",
+            type: "array"
+        },
+        http: {
+            path: "/get-product-same-category",
+            verb: "get"
+
+        }}
+    
+    
+    )
+
+    
+
+
+
+};
+
+ */
