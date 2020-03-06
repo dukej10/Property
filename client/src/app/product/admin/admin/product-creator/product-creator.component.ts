2485cce514f886503dd67b38634b4e7d05638701
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-06 15:45:14 -0500

    form y rear inmueble

--- client/src/app/product/admin/admin/product-creator/product-creator.component.ts
+++ client/src/app/product/admin/admin/product-creator/product-creator.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PropertyModel } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-creator',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pService: ProductService, private route: Router) { }
+
+  product: PropertyModel ={
+    id: null,
+    code: null,
+    name: null,
+    available: null,
+    rooms: null,
+    bathrooms: null,
+    area: null,
+    category: null,
+    price: null,
+    description: null,
+    image: null,
+    image1: null,
+    address: null
+  }
 
   ngOnInit(): void {
+  }
+
+  saveNewProduct(): void{
+    this.pService.saveNewProduct(this.product).subscribe(item =>{
+      alert("se guardo product");
+      this.route.navigate(["/admin/product/list"]);
+    });
+
   }
 
 }
