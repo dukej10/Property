4d4db6b392b6c0ec42f7be2d8909a0d8386835a4
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-06 14:46:32 -0500

    agregar función loopback, y función de busqueda

--- common/models/property.js
+++ common/models/property.js
@@ -1,5 +1,35 @@
 'use strict';
 
 module.exports = function(Property) {
+    Property.getProductsSameAvailable= function (available, cb){
+        Property.find({
+            where:{
+                available:{
+                    eq: available
+                 }
+            }
+        }, cb);
+    
+    };
+
+
+    Property.remoteMethod("getProductsSameAvailable", {
+        accepts: {
+            arg: "available",
+            type: "boolean"
+        },
+        returns: {
+            arg: "properties",
+            type: "array"
+        },
+        http: {
+            path: "/get-product-same-available",
+            verb: "get"
+
+        }}
+    
+    
+    )
+
 
 };
