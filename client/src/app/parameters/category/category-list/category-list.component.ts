05226033caf0a1b08c5cabb0cd11e89de4fc7d25
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-05 22:32:48 -0500

    Eliminar y mostrar botones

--- client/src/app/parameters/category/category-list/category-list.component.ts
+++ client/src/app/parameters/category/category-list/category-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryModel } from 'src/app/models/category.model';
 import { CategoryService } from 'src/app/services/category.service';
+import { Router } from '@angular/router';
+
+declare const deleteMessageModal: any;      /* Mensaje Modal elim */
 
 @Component({
   selector: 'app-category-list',
@@ -9,9 +12,14 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor(private catService: CategoryService) { }
+  constructor(private catService: CategoryService, private route: Router) { }
+
 
   categoryList: CategoryModel[] = [];
+  
+  showConfirmationsButtons: boolean = false;   /*  va a decir si mostrar opciones de confirmar o cancelar si se clickea Eliminar */
+
+  idToShowButtons: string = '';        
 
   ngOnInit(): void {
     this.getAllCategories();
@@ -20,6 +28,20 @@ export class CategoryListComponent implements OnInit {
   getAllCategories(): void{
     this.catService.getAllCategories().subscribe(items => {
       this.categoryList = items;
+    })
+  }
+
+  /* Se usa si se da click en Cancelar para mostrar los botones de confirmar y cancelar */
+  ChangeConfirmationButtons(id){
+    this.idToShowButtons = id;
+    this.showConfirmationsButtons = !this.showConfirmationsButtons;
+  }
+
+  /* Elimina la categoria al dar click en confirmación */
+  deleteCategory(categoryId: string): void{
+    this.catService.deleteCategory(categoryId).subscribe(item => {
+      deleteMessageModal("Ha sido eliminado satisfactoriamente.");
+      this.route.navigate(["/admin/category/list"]);
     })
   }
 
