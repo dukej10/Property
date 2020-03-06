336bf351f96ade13c24d83618c728b5689fd609a
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-06 00:25:44 -0500

    guardar categoria

--- client/src/app/parameters/category/category-creator/category-creator.component.ts
+++ client/src/app/parameters/category/category-creator/category-creator.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryModel } from 'src/app/models/category.model';
+import { CategoryService } from 'src/app/services/category.service';
+import { Router } from '@angular/router';
+
+declare const saveMessageModal: any;      /* Mensaje Modal elim */
 
 @Component({
   selector: 'app-category-creator',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryCreatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private catService: CategoryService, private router: Router) { }
+
+  category: CategoryModel = {
+    code: null,
+    name: null,
+    trequest: null, 
+    id: null  
+  }
 
   ngOnInit(): void {
   }
+
+  /* Guardar la nueva categoria creada */
+  saveNewCategory():void{
+    this.catService.saveNewCategory(this.category).subscribe(item => {
+      console.log("oe");
+      /* saveMessageModal("Ha sido guardada satisfactoriamente."); */
+      alert("oe");
+      console.log("oe");
+      this.router.navigate(["/admin/category/list"]);
+    })
+  }
+
 
 }
