3e6838ac294ec209bbb38680073e9a1793f5beba
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-07 00:05:49 -0500

    login

--- client/src/app/security/login/login.component.ts
+++ client/src/app/security/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,31 +11,27 @@ export class LoginComponent implements OnInit {
 
   fgValidation: FormGroup; /* grupo de validación de formulario */
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private userService: UserService) { 
 
   }
 
-  fgValidationBuilder(){
-    this.fgValidation = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(30), Validators.email]],
-      password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(15)]
+  email: string = '';
+  password: string = '';
+  logged: boolean = false;
+
+
+  ngOnInit() { 
+  }
+  
+
+  onLoginBtnClick(): void{
+    this.userService.loginUser(this.email, this.password).subscribe(item => {
+      console.log(item.id);
+      this.userService.saveToken(item.id);
+      this.userService.saveUserInformation(item.user);
+      location.reload();
     });
   }
 
-  ngOnInit() {
-    this.fgValidationBuilder(); 
-  }
-  
-  loginEvent(){
-    if (this.fgValidation.invalid){
-      alert("Datos Inválidos");
-    }else{
-      alert("yendo al login");
-    }
-  }
-
-  get fgv(){
-    return this.fgValidation.controls;
-  }
 
 }
