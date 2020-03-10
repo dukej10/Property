b724b6537693d3f5df7b76a4a358982f3f8a74fa
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-10 15:47:04 -0500

    .

--- client/src/app/security/register/register.component.ts
+++ client/src/app/security/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ClientModel } from 'src/app/models/client.model';
+import { ClientModule } from 'src/app/client/client.module';
+import { UserModel } from 'src/app/models/user.model';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
   }
 
-}
+  constructor(private catService: UserService, private router: Router, private userService: UserService, private clientS: ClientService) { 
+  }
+
+  
+
+}  
+
+
