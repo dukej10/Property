bbe018fd10ac597b7b84bdc4dc0b3a913e4f0b32
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-10 22:42:14 -0500

    formulario para creación de usuarios

--- client/src/app/security/register/register.component.ts
+++ client/src/app/security/register/register.component.ts
@@ -13,11 +13,90 @@ import { ClientService } from 'src/app/services/client.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  constructor( private router: Router, private userService: UserService, private clientS: ClientService) { 
+    this.categoryFormGroup = this.formGroupCreator();
+  }
+
+  /* OnRegister(){
+    this.userService.registerUser(this.).subscribe(item =>{
+      console.log("Se registró");
+    })
+  } */
+  categoryFormGroup: FormGroup;
+
+  formGroupCreator(): FormGroup{
+     /* los campos que se solicitan */
+    return new FormGroup({ 
+      username : new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+      email: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.email]),
+      name: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]),
+    });
+  }
+
+
+  get username(){
+    return this.categoryFormGroup.get('username');
+  }
+
+  get email(){
+    return this.categoryFormGroup.get('email');
+  }
+
+  get name(){
+    return this.categoryFormGroup.get('name');
+  }
+
+  get password(){
+    return this.categoryFormGroup.get('password');
+  }
+
+
+
   ngOnInit(): void {
   }
 
-  constructor(private catService: UserService, private router: Router, private userService: UserService, private clientS: ClientService) { 
+  buildCategoryData(): UserModel{
+    let user1: UserModel = {
+      id: null,
+    realm: this.name.value,
+    username: this.username.value,
+    email: this.email.value,
+    password: this.password.value,
+    rol: 3,
+    user: null
+    }
+    return user1;
   }
+
+  
+  
+
+
+  /* Guardar la nueva categoria creada usando formgropu, from control name..*/
+  saveNewUser():void{
+    if(this.categoryFormGroup.valid){
+      this.userService.registerUser(this.buildCategoryData()).subscribe(item => {
+        /* saveMessageModal("oeee"); */
+        alert("Se guardó la categoría");
+        this.router.navigate(["/admin/category/list"]);
+      })
+    }else{
+      alert("Llene todos los campos correctamente");
+    }
+    /* Para ngModel */
+    /* this.catService.saveNewCategory(this.category).subscribe(item => {
+      console.log("oe");
+      /* saveMessageModal("Ha sido guardada satisfactoriamente."); 
+      alert("oe");
+      console.log("oe");
+      this.router.navigate(["/admin/category/list"]); 
+    });*/
+
+
+  }
+
 
   
 
