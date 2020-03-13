a421c7f93825f25bfcacd74bc8cf585457cca328
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-13 15:06:50 -0500

    Solicit

---
+++ client/src/app/models/solicitd.model.ts
@@ -0,0 +1,10 @@
+export interface   SolicitModel{
+    id: string;
+    dirreccion: string;
+    tipo: string;
+    valor: number;
+    fecha: string;
+    estado: string;
+    foto: string;
+    asesor: string;
+}
