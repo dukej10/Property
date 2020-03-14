6fbaa4a64568417e8a1b49fdcc43b33ba06198a2
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-14 11:42:22 -0500

    esconder boton

--- client/src/app/template/hero/hero.component.ts
+++ client/src/app/template/hero/hero.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-hero',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private uService: UserService) { }
+
+  userLogged: boolean = false;
 
   ngOnInit(): void {
+    this.showButton();
+  }
+
+  showButton(): void{
+    let userInfo = this.uService.getUserInformation();
+    if(isNullOrUndefined(userInfo)){ 
+      this.userLogged = false;  /* Si el usuario no está logueado */
+    }else{ 
+      this.userLogged = true;  /* Si el usuario está logueado */
+    }
+
   }
 
 }
